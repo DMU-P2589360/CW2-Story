0e1de305b21e1b72129d97dd71c9bcdc48157c06
DMU-P2589360 <92032248+DMU-P2589360@users.noreply.github.com>
2023-05-01 19:40:41 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+playAudio = () => { //Function created to play audio 
+    document.getElementById ("audioPlayer").play(); //Links to audio location 
+    }
+    stopAudio = () => { //Function created to play audio 
+    document.getElementById ("audioPlayer").pause(); //Links to audio location 
+    }
+    playAudio2 = () => { //Function created to play audio 
+    document.getElementById ("audioPlayer2").play(); //Links to audio location 
+    }
+    stopAudio2 = () => { //Function created to play audio 
+    document.getElementById ("audioPlayer2").pause(); //Links to audio location 
+    }
+    playAudio3 = () => { //Function created to play audio 
+        document.getElementById ("audioPlayer3").play(); //Links to audio location 
+        }
+        stopAudio3 = () => { //Function created to play audio 
+        document.getElementById ("audioPlayer3").pause(); //Links to audio location 
+        }
+        playAudio4 = () => { //Function created to play audio 
+            document.getElementById ("audioPlayer4").play(); //Links to audio location 
+            }
+            stopAudio4 = () => { //Function created to play audio 
+                document.getElementById ("audioPlayer3").pause(); //Links to audio location 
+                }
+    
+function buttonAudio() {
+    document.getElementById ("buttonAudio").pause(); //Links to audio location  
+}
+function nextAudio() {
+ 
+}
+function prevAudio() {
+ 
+}
+    document.getElementById(".text").addEventListener("mouseover", playAudio); //Links to audio location 
+    document.getElementById(".text").addEventListener("mouseout", stopAudio); //Links to audio location 
+    document.getElementById(".text2").addEventListener("mouseover", playAudio); //Links to audio location 
+    document.getElementById(".text").addEventListener("mouseout", stopAudio); //Links to audio location 
+    document.getElementById(".text3").addEventListener("mouseover", playAudio); //Links to audio location 
+    document.getElementById(".text3").addEventListener("mouseout", stopAudio); //Links to audio location 
+    document.getElementById(".text4").addEventListener("mouseover", playAudio); //Links to audio location 
+    document.getElementById(".text4").addEventListener("mouseout", stopAudio); //Links to audio location 
